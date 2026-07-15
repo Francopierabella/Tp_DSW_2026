30d5e23de415256970a177305c0f67068aca059b
Franco <francopierabella10@gmail.com>
2026-07-15 10:35:40 -0300

    Changes in MD, ready to start with mikroOrm

---
+++ src/shared/db/orm.ts
@@ -0,0 +1,23 @@
+import { MikroORM } from "@mikro-orm/core";
+
+import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
+
+export const orm = await MikroORM.init({
+    entities: ['dist/**/*.entity.js'],
+    entitiesTs: ['src/**/*.entity.ts'],
+    dbName: 'farmacia',
+    type:'mysql',
+    clientUrl:'mysql://root:franpier10@localhost:3306/farmacia',
+    highlighter: new SqlHighlighter(),
+    debug: true,
+    schemaGenerator:{
+        disableForeignKeys: true,
+        createForeignKeyConstraints:true,
+        ignoreSchema:[],
+    },
+})
+
+export const syncSchema = async () => {
+    const generator = orm.getSchemaGenerator();
+    await generator.updateSchema();
+}
