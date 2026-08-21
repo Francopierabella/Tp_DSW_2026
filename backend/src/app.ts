edbbc47469d51be5e02bede681f480e1401b4241
Franco <francopierabella10@gmail.com>
2026-08-21 11:40:34 -0300

    CRUD ProductCategory ready to check

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,21 +6,28 @@ import dotenv from 'dotenv';
 import { orm , syncSchema} from './shared/db/orm.js';
 import { RequestContext } from '@mikro-orm/core';
 import cors from "cors";
+// CORS (Cross-Origin Resource Sharing) es un mecanismo de seguridad de los navegadores web.
+//  Permite que una página web pida datos a un servidor que está en un dominio, puerto o protocolo diferente al de la página.
 
 dotenv.config();
 const app = express();
+// registra un middleware de Express.
+// Un middleware es una función que se ejecuta durante el procesamiento de una petición.
 app.use(cors());
 
 const PORT = 3000;
 app.use(express.json());
 
-// luego de los middlewares base , como express y .json,+
 
 app.use((req,res,next) => {
     RequestContext.create(orm.em, next); // em => entity manager
 })
 
-//  y antes de los use de los routers, sincronizamos el esquema de la base de datos
+// Se crea un contexto de MikroORM para cada petición HTTP.
+// Esto permite que el EntityManager (orm.em) gestione las entidades
+// y operaciones de base de datos dentro del contexto de esa petición.
+// Una vez creado el contexto, next() continúa con el siguiente middleware o ruta.
+
 
 // app.use("/api/customers",customerRouter);
 app.use("/api/productCategories",productCategoryRouter);
